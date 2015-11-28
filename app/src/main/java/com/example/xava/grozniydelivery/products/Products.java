0199ae7b82b2c3b8d55ad196aa033e88bdfdd0c3
Mage <maga1976@gmail.com>
2015-11-28 14:45:15 +0300

    NavigationView was added

---
+++ app/src/main/java/com/example/xava/grozniydelivery/products/Products.java
@@ -0,0 +1,37 @@
+package com.example.xava.grozniydelivery.products;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import com.example.xava.grozniydelivery.ImageAdapter;
+import com.example.xava.grozniydelivery.ImageItem;
+import com.example.xava.grozniydelivery.R;
+
+import java.util.ArrayList;
+
+public class Products extends AppCompatActivity {
+ListView lvProducts;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_products);
+
+        int[] imageList = new int[]{R.drawable.ic_all_stores,R.drawable.ic_gastronom,R.drawable.ic_vegetables,
+        R.drawable.ic_fruits,R.drawable.ic_water_tea_cofee};
+
+        lvProducts = (ListView)findViewById(R.id.lvProducts);
+        ArrayList<ImageItem> items = new ArrayList<ImageItem>();
+        ImageAdapter adapter = new ImageAdapter(this,items);
+        lvProducts.setAdapter(adapter);
+
+        for (int i = 0; i< imageList.length; i++){
+
+            ImageItem newImage = new ImageItem(imageList[i]);
+            adapter.add(newImage);
+        }
+
+
+
+    }
+}
